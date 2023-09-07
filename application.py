c84bd780ee18d9e38d051d605f5199a40e3d1d9f
hamza <muhammadmehmood2004@gmail.com>
2023-09-07 11:44:27 +0100

    yhyh

--- application.py
+++ application.py
@@ -1,8 +1,42 @@
-from flask import Flask 
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+
 
 application = Flask(__name__)
 
-@application.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+UPLOAD_FOLDER = "static/images"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route('/')
+def index():
+    files = os.listdir(UPLOAD_FOLDER)
+    images = [file for file in files if allowed_file(file)]
+    return render_template('index.html', images=images)
+
+
+@application.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect('/')
+    file = request.files['file']
+    if file.filename == '':
+        return redirect('/')
+    if file and allowed_file(file.filename):
+        filename = os.path.join(
+            application.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        return redirect('/')
+    return redirect('/')
+
 
